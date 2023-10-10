d62618e04fd906d324f4b37038fcb373d478bc91
Marisa Kirisame <marisa@cs.utah.edu>
2023-10-10 08:34:10 +0000

    save

---
+++ single_eval.py
@@ -0,0 +1,42 @@
+import subprocess
+
+raise
+
+PROFILE = False
+
+profile_header = "heaptrack " if PROFILE else ""
+def cleanup():
+    subprocess.run("../bin/zkCli.sh deleteall /x", shell=True)
+    subprocess.run("pkill -f noria-server", shell=True)
+    subprocess.run("pkill -f heaptrack", shell=True)
+
+# does not call cleanup - have to call it yourself.
+def run(mb):
+    subprocess.Popen(f"RUST_BACKTRACE=1 USE_ZOMBIE=1 {profile_header}./target/release/noria-server --deployment x --memory {mb * 1024 * 1024} --durability memory", shell=True)
+    #subprocess.Popen(f"./target/release/noria-server --deployment x --durability memory", shell=True)
+    result = subprocess.run("timeout 20m ./target/release/lobsters-noria --deployment x --scale 10 --prime --warmup 300 --runtime 300", shell=True, capture_output=True, text=True)
+    print("printing result...")
+    print(result.stdout)
+    print("printing result err...")
+    print(result.stderr)
+    print("for loop...")
+    for x in result.stdout.splitlines():
+        print(x)
+        result_pattern = "# generated ops/s:"
+        if x.startswith(result_pattern):
+            cleanup()
+            print(float(x[len(result_pattern):]))
+            raise 
+
+cleanup()
+subprocess.run("cargo update", shell=True, check=True)
+subprocess.run("cargo build --release --bin noria-server", shell=True, check=True)
+subprocess.run("cargo build --release --bin lobsters-noria", shell=True, check=True)
+
+
+run(1000)
+cleanup()
+#run(10000)
+#cleanup()
+#run(256)
+#cleanup()
